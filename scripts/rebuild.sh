14c653996cf7faebfdecb23d5f80376b050eaf74
Evgeny Budilovsky <budevg@gmail.com>
2023-11-18 18:24:26 +0200

    build xmobar as binary

--- scripts/rebuild.sh
+++ scripts/rebuild.sh
@@ -3,8 +3,4 @@ set -x
 
 cd ~/.xmonad
 
-if [ ! -f xmobar ]; then
-    stack --nix --local-bin-path=`pwd` install xmobar-0.44.2
-fi
-
 stack --nix --local-bin-path=`pwd` install
--- src/xmobar.hs
+++ src/xmobar.hs
@@ -1,24 +1,34 @@
-Config
-  { font = "-*-Fixed-Bold-R-Normal-*-13-*-*-*-*-*-*-*"
-  , bgColor = "black"
-  , fgColor = "grey"
-  , position = TopW L 90
-  , commands = [ Run Cpu ["-L","3","-H","50","--normal","green","--high","red"] 10
-               , Run Memory ["-t","Mem: <usedratio>%"] 10
-               , Run Date "%a %b %_d %H:%M " "date" 10
-               , Run Com "/bin/bash" ["-c", "~/.xmonad/scripts/get-volume.sh"] "volume" 10
-               , Run Battery [ "-t", "<acstatus>: <left>%"
-                             , "-L", "25", "-l", "red"
-                             , "-H", "80", "-h", "green"
-                             , "-n", "yellow"
-                             , "--"
-                             , "-O", "AC"
-                             , "-o", "Bat"
-                             ] 10
-               , Run StdinReader
-               ]
-  , sepChar = "%"
-  , alignSep = "}{"
-  -- , template = "%StdinReader% }{ %cpu% | %memory% | Vol: %volume% | <fc=#ee9a00>%date%</fc>"
-  , template = "%StdinReader% }{ %battery% | %cpu% | %memory% | Vol: %volume% | <fc=#ee9a00>%date%</fc>"
-  }
+import Xmobar
+
+config =
+  defaultConfig
+    { font = "-*-Fixed-Bold-R-Normal-*-13-*-*-*-*-*-*-*"
+    , bgColor = "black"
+    , fgColor = "grey"
+    , position = TopW L 90
+    , commands =
+        [ Run $ Cpu ["-L", "3", "-H", "50", "--normal", "green", "--high", "red"] 10
+        , Run $ Memory ["-t", "Mem: <usedratio>%"] 10
+        , Run $ Date "%a %b %_d %H:%M " "date" 10
+        , Run $ Com "/bin/bash" ["-c", "~/.xmonad/scripts/get-volume.sh"] "volume" 10
+        , Run $
+            Battery
+              [ "-t" , "<acstatus>: <left>%"
+              , "-L", "25", "-l", "red"
+              , "-H", "80", "-h", "green"
+              , "-n", "yellow"
+              , "--"
+              , "-O", "AC"
+              , "-o", "Bat"
+              ]
+              10
+        , Run $ StdinReader
+        ]
+    , sepChar = "%"
+    , alignSep = "}{"
+    , -- , template = "%StdinReader% }{ %cpu% | %memory% | Vol: %volume% | <fc=#ee9a00>%date%</fc>"
+      template = "%StdinReader% }{ %battery% | %cpu% | %memory% | Vol: %volume% | <fc=#ee9a00>%date%</fc>"
+    }
+
+main :: IO ()
+main = xmobar config
